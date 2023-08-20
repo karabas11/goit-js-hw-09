f9c24d840095771ec474541f1b876fefccc135e0
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-08-20 17:55:20 +0300

    HW JS 09 qustion 1
    HW JS 09 qustion 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startButton.addEventListener('click', onStart);
+stopButton.addEventListener('click', onStop);
+
+let timerId = null;
+
+
+function onStart(){
+  startButton.setAttribute('disabled', true);
+  stopButton.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    body.style.backgroundColor =  getRandomHexColor();
+  }, 1000)
+  
+};
+
+function onStop(){
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+  clearInterval(timerId);
+};
+
+function getRandomHexColor() {
+  return #${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)};
+}
 
