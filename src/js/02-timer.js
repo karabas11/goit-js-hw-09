7ba3f18a51276c06d1101aec2dc93d26a6276b09
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-08-20 18:03:00 +0300

    HW JS 09 question 2, 3
    HW JS 09 question 2, 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,85 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const refs = {
+  timer : document.querySelector('.timer'),
+  input : document.querySelector('#datetime-picker'),
+  buttonStart : document.querySelector('button'),
+  dataDay : document.querySelector('[data-days]'),
+  dataHours : document.querySelector('[data-hours]'),
+  dataMinutes : document.querySelector('[data-minutes]'),
+  dataSecondes : document.querySelector('[data-seconds]'),
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0].getTime();
+    const currentData = Date.now();
+    
+    if(selectedDate < currentData){
+      refs.buttonStart.setAttribute('disabled', true);
+      // alert ("Please choose a date in the future") ;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      refs.buttonStart.removeAttribute('disabled');
+      refs.buttonStart.addEventListener('click', onStartTimer);
+
+      function onStartTimer() {
+        refs.buttonStart.setAttribute('disabled', true);
+
+        const timerId = setInterval(() => {
+          const currentTime = Date.now();       
+          const deltaTime =  selectedDate - currentTime;
+
+          if(deltaTime <= 0){
+            clearInterval(timerId);
+            console.log(timerId);
+            return;
+          }else{
+            const { days, hours, minutes, seconds } = convertMs(deltaTime);
+          // console.log(`${days}:${hours}:${minutes}:${seconds}`);
+          updateTimerFase({ days, hours, minutes, seconds });
+          }
+           }, 1000)
+      }
+    }
+  },
+};
+
+flatpickr(refs.input, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerFase({days, hours, minutes, seconds}) {
+  refs.dataDay.textContent = ${days};
+  refs.dataHours.textContent = ${hours};
+  refs.dataMinutes.textContent = ${minutes};
+  refs.dataSecondes.textContent = ${seconds};
+
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  input: document.querySelector('input'),
+};
+
+refs.form.addEventListener('submit', onFormData);
+// refs.buttonStart.addEventListener('click', onStart);
+
+function onFormData(evt) {
+  evt.preventDefault();
+  const {amount, delay, step} = evt.currentTarget.elements;
+
+  const data = {
+    amount: amount.value,
+    delay: delay.value,
+    step: step.value,
   }
-}
+
+  for(let i = 0; i < data.amount; i += 1){
+    const position = i;
+    const delay = Number(data.delay) + Number(position*data.step);
+    
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      }
+};
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject)=>{
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+      // Fulfill
+      resolve ({position, delay});
+      } else {
+      // Reject
+      reject({position, delay});
+      }
+      }, delay);
+  });
+};
